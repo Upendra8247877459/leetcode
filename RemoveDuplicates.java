d85e4e34ed893f75a84ac56388026b54a0bd1ff7
Narala Upendra <naralauppendra@gmail.com>
2026-07-31 23:48:47 +0530

    Add optimized removeDuplicates method
    Implemented O(n) optimization for removing duplicates using a two pointers pattern.

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -20,3 +20,20 @@ class Solution {
         return count;
     }
 }
+
+// O(n) optimization using two pointers pattern
+class Solution {
+    public int removeDuplicates(int[] nums) {
+      int i=0,j=1;
+      while(j<nums.length) {
+        if(nums[i]==nums[j]) {
+            j++;
+        }
+        else {
+            i++;
+            nums[i]=nums[j];
+        }
+      }
+      return i+1;
+    }
+}
