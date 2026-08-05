9225f78a3b3c77710abb81612dae6495c7901c3e
Narala Upendra <naralauppendra@gmail.com>
2026-08-05 22:43:44 +0530

    Optimize containsDuplicate with HashSet
    Refactor containsDuplicate method to use HashSet for O(n) optimization.

---
+++ ContainsDuplicate.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+       for (int i = 0; i < nums.length; i++) {
+        for (int j = i + 1; j < nums.length; j++) {
+          if (nums[i] == nums[j]) {
+            return true;
+             }
+        }
+       }
+    return false;
+    }
+}
+
+
+// Using Hashset to get O(n) optimization
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
